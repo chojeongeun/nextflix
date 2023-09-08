dbe1fc5c086998ef8fd2facad159d5f083235192
chojeongeun <juy0816@naver.com>
2023-09-08 11:01:19 +0900

    기본 메인 페이지에 헤더 연결 및 페이지 타입 지정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
-import Image from 'next/image';
+import Header from '@/component/Header';
+import type { NextPage } from 'next';
+import Head from 'next/head';
 
-export default function Home() {
+const Home: NextPage = () => {
 	return (
-		<main>
-			<h1>NEXTFLIX</h1>
-		</main>
+		<div>
+			<Head>
+				<title>NEXTFLIX</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<Header />
+			<main>
+				<h1>NEXTFLIX</h1>
+			</main>
+		</div>
 	);
-}
+};
+
+export default Home;
