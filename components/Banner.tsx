3485af043a3112e0a3b1be58b54e2b5b34c8923d
chojeongeun <juy0816@naver.com>
2023-09-11 11:52:01 +0900

    영화 텍스트 정보 출력 및 스타일링 & 배너컴포넌트 버튼 스타일링 커스텀 클래스로 그룹지정

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,6 +2,7 @@ import { Movie } from '@/types';
 import { baseURL } from '@/url';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
+import { FaInfoCircle, FaPlay } from 'react-icons/fa';
 
 interface Props {
 	original: Movie[];
@@ -16,10 +17,42 @@ function Banner({ original }: Props) {
 	}, [original]);
 
 	return (
-		<section>
-			<div>
-				<Image src={`${baseURL}${Movie?.backdrop_path}`} alt={`${Movie?.title || Movie?.name}`} fill priority />
-			</div>
+		<section className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
+			{Movie && (
+				<>
+					{/* pic frame */}
+					<div className='absolute top-0 left-0 z-[1] h-[95vh] w-full'>
+						<Image
+							src={`${baseURL}${Movie?.backdrop_path}`}
+							alt={`${Movie?.title || Movie?.name}`}
+							fill
+							priority
+							quality={50}
+							className='object-cover'
+						/>
+					</div>
+
+					{/* title */}
+					<h1 className='relative z-[3] text-2xl font-bold drop-shadow md:text-4xl lg:text-7xl'>
+						{Movie?.title || Movie?.name}
+					</h1>
+
+					{/* overview */}
+					<p className='relative z-[3] text-xs max-w-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl'>
+						{Movie?.overview}
+					</p>
+
+					{/* button set */}
+					<nav className='relative z-[3] flex space-x-s'>
+						<button className='bannerButton'>
+							<FaPlay /> Play
+						</button>
+						<button className='bannerButton'>
+							<FaInfoCircle /> More Info
+						</button>
+					</nav>
+				</>
+			)}
 		</section>
 	);
 }
