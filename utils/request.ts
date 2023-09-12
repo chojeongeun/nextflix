3298337eee06b1ff06e91fc4707de6421a25bcd7
chojeongeun <juy0816@naver.com>
2023-09-12 09:39:44 +0900

    이미지 사이즈 호출시 크기 지정

--- utils/request.ts
+++ utils/request.ts
@@ -2,7 +2,7 @@ const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 const requests = {
-	original: `${BASE_URL}/trending/all/day?with_networks=213&api_key=${API_KEY}`,
+	original: `${BASE_URL}/trending/all/day?with_networks=Netflix&api_key=${API_KEY}`,
 	top: `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&languages=en-US`,
 	sf: `${BASE_URL}/discover/movie?api_key=${API_KEY}&languages=en-US&with_genres=878`,
 	drama: `${BASE_URL}/discover/movie?api_key=${API_KEY}&languages=en-US&with_genres=18`,
